08ab25d4da9f1a046cbb60be1ef2f7567eb4aee5
22sook00 <22sook00@gmail.com>
2023-03-25 23:19:34 +0900

    Add custom checkbox color

--- src/components/Common/Checkbox/Checkbox.tsx
+++ src/components/Common/Checkbox/Checkbox.tsx
@@ -6,7 +6,12 @@ interface CheckboxProps {
 const Checkbox: FC<CheckboxProps> = ({ text }) => {
   return (
     <label htmlFor={text} className=" flex items-center select-none	">
-      <input type="checkbox" id={text} name={text} />
+      <input
+        id={text}
+        name={text}
+        type="checkbox"
+        className="formtick appearance-none h-6 w-6 border-solid border-2 border-gray-default rounded-md checked:bg-point-gr-light checked:border-transparent focus:outline-none"
+      />
       <p className="ml-2">{text}</p>
     </label>
   );
